3194b3edd57372bc8981a83ff7c7e7b04559bbcf
Krista Opsahl-Ong <kristaopsahlong@gmail.com>
2017-10-16 12:03:22 -0400

    tested out workflow for labeling

--- precise_labeling/threat_localization.py
+++ precise_labeling/threat_localization.py
@@ -3,14 +3,17 @@ sys.path.insert(0, '../visualize/')
 
 def main(argv):
 	if len(argv) != 3:
-		print("Please input 2 files to combine.")
+		print("Please input the file id and region.")
 	else:
-		file1 = open(argv[1], "r")
+		file1_directory = "xyfiles/"+argv[1]+"_xy_"+argv[2]+".txt"
+		file2_directory = "xyfiles/"+argv[1]+"_xy_"+argv[2]+"_slice.txt"
+
+		file1 = open(file1_directory, "r")
 		file1_points = file1.readlines()
 		file1_points = [x.rstrip('\n') for x in file1_points]
 		file1.close() 
 
-		file2 = open(argv[2], "r")
+		file2 = open(file2_directory, "r")
 		depth1 = file2.readline().split('\t')[1]
 		file2.readline()
 		depth2 = file2.readline().split('\t')[1]
@@ -18,7 +21,7 @@ def main(argv):
 		depths = [depth1,depth2]
 		file2.close()
 
-		output = open(argv[1].split('_')[0] + "_" + argv[1].split('_')[2].split(".")[0] + "_threatcube" + ".txt", "w")
+		output = open(file1_directory.split('_')[0] + "_" + file1_directory.split('_')[2].split(".")[0] + "_threatcube" + ".txt", "w")
 		for d in depths:
 			for l in file1_points:
 				output.write(l + "	" + d)
