84a55f00ce6087bd0cc1fd4edd7b875737c46a39
Patrick Gray <clifgray@Patricks-MacBook-Pro-3.local>
2017-10-15 22:12:55 -0400

    adding precise labeling dir with instructions and scripts.

--- precise_labeling/get_slice.py
+++ precise_labeling/get_slice.py
@@ -1,25 +1,25 @@
 import sys
+sys.path.insert(0, '../visualize/')
 from classes import *
 
 
 def main(argv):
-	if len(argv) != 3:
-		print("please intput the first xy coordinates file and the region containting the violation")
+	if len(argv) != 2:
+		print("Please input the xy coordinates file.")
 	else:
 		file1 = open(argv[1], "r")
 		first_y = int(file1.readline().split('\t')[1])
-		print(first_y)
 		file1.readline()
 		third_y = int(file1.readline().split('\t')[1])
-		print(third_y)
-		nthSlice = 660 - (first_y + third_y)/2
-		print(nthSlice)
+		nthSlice = int(660 - (first_y + third_y)/2)
 		file1.close() 
 
-		bs = BodyScan(argv[1].split('_')[0]+".a3d")
+		a3d_path = "a3d/" + argv[1].split('_')[0].split("/")[1] + ".a3d"
+		bs = BodyScan(a3d_path)
 		slc = bs.get_single_image(nthSlice)
 
-		fileName = argv[1].split('.')[0] + "_slice.png"
+		changed_path = "slices/" + argv[1].split("/")[1]
+		fileName = changed_path.split('.')[0] + "_slice.png"
 		bs.write_square_slice_to_img(slc,fileName)
 
 
--- precise_labeling/threat_localization.py
+++ precise_labeling/threat_localization.py
@@ -1,9 +1,9 @@
 import sys
-
+sys.path.insert(0, '../visualize/')
 
 def main(argv):
 	if len(argv) != 3:
-		print("please intput 2 files to combine")
+		print("Please input 2 files to combine.")
 	else:
 		file1 = open(argv[1], "r")
 		file1_points = file1.readlines()
@@ -18,10 +18,10 @@ def main(argv):
 		depths = [depth1,depth2]
 		file2.close()
 
-		output = open(argv[1].split('_')[0] + "_threat" + argv[2].split("_")[1] + ".txt", "w")
+		output = open(argv[1].split('_')[0] + "_" + argv[1].split('_')[2].split(".")[0] + "_threatcube" + ".txt", "w")
 		for d in depths:
 			for l in file1_points:
-				output.write(l + "	" + d + "\n")
+				output.write(l + "	" + d)
 		output.close()
 
 
