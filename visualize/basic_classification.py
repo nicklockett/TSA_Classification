d7379b72f2fd8845a3812ccf88a45d207007aba7
Patrick Gray <clifgray@Patricks-MacBook-Pro-3.local>
2017-11-09 21:12:26 -0500

    making basic classifiers 2d for the time being

--- visualize/basic_classification.py
+++ visualize/basic_classification.py
@@ -6,83 +6,35 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans, AffinityPropagation
 
+from basic_cnn import extract2DDataSet
+
 import matplotlib.pyplot as plt
 
+block_size = 44
+# Set segmentNumber
+segmentNumber = 0.8
+
+sc = SupervisedClassifier('../../stage1_labels.csv')
+
 # need to ingest and iterate through multiple bodies
-images_list = [
-	"../precise_labeling/a3d/1e4a14d2e1eb381b773446de1c0c0b7e.a3d"
+image_path_list = [
+	"../precise_labeling/a3d/1e4a14d2e1eb381b773446de1c0c0b7e.a3d",
+	#"../precise_labeling/a3d/1e5f7a00ff4f02b0de0ea97139b6c92e.a3d"
 ]
 
-### Formating Data ###
+# Load training and eval data
+(train_data, train_labels, eval_data, eval_labels) = extract2DDataSet(image_path_list, block_size, segmentNumber, sc)
+flattened_train = []
+for item in train_data:
+	flattened_train.append(item.flatten())
+train_data = flattened_train
 
-# right now we're just throwing all the blocks from all images into the same list which is fed to the classifier
-uniform_blocks = []
-sc = SupervisedClassifier('../data/data/stage1_labels.csv')
+flattened_eval = []
+for item in eval_data:
+	flattened_eval.append(item.flatten())
+eval_data = flattened_eval
 
-for file_path in images_list:
-	print(file_path)
-	bs = BodyScan(file_path)
-	print('made body scan!')
-	bsg = BlockStreamGenerator(bs, sc, blockSize = 36)
-	#block_list = bsg.generateStream()
-	block_list = bsg.generateStreamHandLabeled()
-	print('generated blocks!')
-	
-	
-	for block in block_list:
-		# checking to make sure it is a full block and not an edge case
-		if block.data.shape == (block.n, block.n, block.n):
-			uniform_blocks.append(block)
-	
-# shuffling so that the subset below doesn't bias the training data
-shuffle(uniform_blocks)
 
-# subset image into test and training data
-# TODO would this will be more effective to subset whole images into test vs training?
-block_count = len(uniform_blocks)
-
-training_data = uniform_blocks[:int(block_count/2)]
-print('training data: ', len(training_data))
-test_data = uniform_blocks[int(block_count/2):]
-print('test data: ', len(test_data))
-
-# flatten the blocks into a vector
-
-# training data
-for block in training_data:
-	block.data = block.data.flatten()
-		
-print 'flattened training data'
-
-final_data_train = []
-for block in training_data:
-	final_data_train.append(block.data)
-
-print('created final data training set')
-
-final_labels = []
-for block in training_data:
-	final_labels.append(block.threat)
-
-print('appended final labels to training data')
-
-# test data
-for block in test_data:
-	block.data = block.data.flatten()
-
-print('flattened test data')
-
-final_data_test = []
-for block in test_data:
-	final_data_test.append(block.data)
-
-print('created final test data set')
-
-final_labels_test = []
-for block in test_data:
-	final_labels_test.append(block.threat)
-
-print('appended final labels to test data')
 
 ###### Dimensionality Reduction ######
 
@@ -90,9 +42,9 @@ print('appended final labels to test data')
 """
 print("--- PCA Output ---")
 
-print(len(final_data_test + final_data_train))
+print(len(eval_data + train_data))
 pca = PCA(n_components=3)
-pca.fit(final_data_test + final_data_train)
+pca.fit(eval_data + train_data)
 
 print(pca.components_.shape)
 print("Explained variance per component:")
@@ -104,36 +56,35 @@ plt.show()
 
 
 ###### Unsupervised Learning ######
-
+"""
 ### K-Means ###
-
+"""
 print("--- K-Means Output ---")
-kmeans_classifier = KMeans(n_clusters=2, random_state=0).fit(final_data_train)
+kmeans_classifier = KMeans(n_clusters=2, random_state=0).fit(train_data)
 print("Done training.")
 
-predicted = kmeans_classifier.predict(final_data_test)
+predicted = kmeans_classifier.predict(eval_data)
 
 # output results
 print("Classification report for classifier %s:\n%s\n"
-      % (kmeans_classifier, metrics.classification_report(final_labels_test, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(final_labels_test, predicted))
+      % (kmeans_classifier, metrics.classification_report(eval_labels, predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(eval_labels, predicted))
 
 ### AffinityPropagation ###
 # WARNING: this is super memory intensive
 print("--- AffinityPropagation Output ---")
-affinity_classifier = AffinityPropagation(max_iter=50).fit(final_data_train)
+affinity_classifier = AffinityPropagation(max_iter=50).fit(train_data)
 print("Done training.")
 
-predicted = affinity_classifier.predict(final_data_test)
+predicted = affinity_classifier.predict(eval_data)
 
 # output results
 print("Classification report for classifier %s:\n%s\n"
-      % (affinity_classifier, metrics.classification_report(final_labels_test, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(final_labels_test, predicted))
+      % (affinity_classifier, metrics.classification_report(eval_labels, predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(eval_labels, predicted))
 
-"""
 ###### Supervised Learning ######
-
+"""
 ### SVM ###
 print("--- SVM Output ---")
 
@@ -141,17 +92,16 @@ print("--- SVM Output ---")
 svm_classifier = svm.SVC(gamma=0.001)
 
 # we learn on the training data
-svm_classifier.fit(final_data_train, final_labels)
+svm_classifier.fit(train_data, train_labels)
 print("Done training.")
 
 # now predict the threat
-predicted = svm_classifier.predict(final_data_test)
+predicted = svm_classifier.predict(eval_data)
 
 # output results
 print("Classification report for classifier %s:\n%s\n"
-      % (svm_classifier, metrics.classification_report(final_labels_test, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(final_labels_test, predicted))
-
+      % (svm_classifier, metrics.classification_report(eval_labels, predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(eval_labels, predicted))
 
 ### Basic Decision Tree ###
 print("--- Decision Tree Output ---")
@@ -159,18 +109,17 @@ print("--- Decision Tree Output ---")
 tree_classifier = tree.DecisionTreeClassifier()
 
 #learn
-tree_classifier = tree_classifier.fit(final_data_train, final_labels)
+tree_classifier = tree_classifier.fit(train_data, train_labels)
 print("Done training.")
 
 # predict
-predicted = tree_classifier.predict(final_data_test)
+predicted = tree_classifier.predict(eval_data)
 
 # output results
 print("Classification report for classifier %s:\n%s\n"
-      % (tree_classifier, metrics.classification_report(final_labels_test, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(final_labels_test, predicted))
+      % (tree_classifier, metrics.classification_report(eval_labels, predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(eval_labels, predicted))
 
-"""
 ### RandomForestClassifier ###
 
 print("--- RandomForestClassifier Output ---")
@@ -178,32 +127,33 @@ print("--- RandomForestClassifier Output ---")
 forest_classifier = RandomForestClassifier(n_estimators=10)
 
 #learn
-forest_classifier = forest_classifier.fit(final_data_train, final_labels)
+forest_classifier = forest_classifier.fit(train_data, train_labels)
 print("Done training.")
 
 # predict
-predicted = forest_classifier.predict(final_data_test)
+predicted = forest_classifier.predict(eval_data)
 
 # output results
 print("Classification report for classifier %s:\n%s\n"
-      % (forest_classifier, metrics.classification_report(final_labels_test, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(final_labels_test, predicted))
+      % (forest_classifier, metrics.classification_report(eval_labels, predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(eval_labels, predicted))
 
+"""
 print("--- AdaBoostClassifier Output ---")
 
 ada_classifier = AdaBoostClassifier(n_estimators=100)
 
 #learn
-ada_classifier = ada_classifier.fit(final_data_train, final_labels)
+ada_classifier = ada_classifier.fit(train_data, train_labels)
 print("Done training.")
 
 # predict
-predicted = ada_classifier.predict(final_data_test)
+predicted = ada_classifier.predict(eval_data)
 
 # output results
 print("Classification report for classifier %s:\n%s\n"
-      % (ada_classifier, metrics.classification_report(final_labels_test, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(final_labels_test, predicted))
+      % (ada_classifier, metrics.classification_report(eval_labels, predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(eval_labels, predicted))
 
 
 ### Gaussian Naive Bayes ###
@@ -211,30 +161,30 @@ print("--- Gaussian Naive Bayes Output ---")
 
 gnb_classifier = naive_bayes.GaussianNB()
 
-gnb_classifier = gnb_classifier.fit(final_data_train, final_labels)
+gnb_classifier = gnb_classifier.fit(train_data, train_labels)
 print("Done training.")
 
-predicted = gnb_classifier.predict(final_data_test)
+predicted = gnb_classifier.predict(eval_data)
 
 # output results
 print("Classification report for classifier %s:\n%s\n"
-      % (gnb_classifier, metrics.classification_report(final_labels_test, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(final_labels_test, predicted))
+      % (gnb_classifier, metrics.classification_report(eval_labels, predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(eval_labels, predicted))
 
 ### Multinomial Naive Bayes ###
 print("--- Multinomial Naive Bayes Output ---")
 
 mnb_classifier = naive_bayes.MultinomialNB()
 
-mnb_classifier = mnb_classifier.fit(final_data_train, final_labels)
+mnb_classifier = mnb_classifier.fit(train_data, train_labels)
 print("Done training.")
 
-predicted = mnb_classifier.predict(final_data_test)
+predicted = mnb_classifier.predict(eval_data)
 
 # output results
 print("Classification report for classifier %s:\n%s\n"
-      % (mnb_classifier, metrics.classification_report(final_labels_test, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(final_labels_test, predicted))
+      % (mnb_classifier, metrics.classification_report(eval_labels, predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(eval_labels, predicted))
 
 
 ### Basic Multi-Layer Perceptron ###
@@ -242,10 +192,10 @@ print("Confusion matrix:\n%s" % metrics.confusion_matrix(final_labels_test, pred
 from sklearn.preprocessing import StandardScaler  
 scaler = StandardScaler()  
 # Don't cheat - fit only on training data
-scaler.fit(final_data_train)  
-scaled_train = scaler.transform(final_data_train)  
+scaler.fit(train_data)  
+scaled_train = scaler.transform(train_data)  
 # apply same transformation to test data
-scaled_test = scaler.transform(final_data_test)  
+scaled_test = scaler.transform(eval_data)  
 
 print("--- Basic Multi-Layer Perceptron Output ---")
 from sklearn.neural_network import MLPClassifier
@@ -253,14 +203,14 @@ from sklearn.neural_network import MLPClassifier
 
 mlp_classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
 
-mlp_classifier.fit(scaled_train, final_labels)
+mlp_classifier.fit(scaled_train, train_labels)
 print("Done training.")
 
 predicted = mlp_classifier.predict(scaled_test)
 
 # output results
 print("Classification report for classifier %s:\n%s\n"
-      % (mlp_classifier, metrics.classification_report(final_labels_test, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(final_labels_test, predicted))
+      % (mlp_classifier, metrics.classification_report(eval_labels, predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(eval_labels, predicted))
 
 """
