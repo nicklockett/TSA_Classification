c5d5ccbfe397d976114938bcaa535f8d806412e2
Krista Opsahl-Ong <kristaopsahlong@gmail.com>
2017-11-13 11:07:14 -0500

    changed filepath

--- visualize/dataExtraction.py
+++ visualize/dataExtraction.py
@@ -191,6 +191,8 @@ class BlockStreamGenerator:
                         filepath_max = "generated_blocks/block_size_56/max/"
                         filepath_max = "generated_blocks/block_size_56/sum/"
                         filepath_max = "generated_blocks/block_size_56/var/"
+                        print 'region label: ', str(region_label)[1:]
+                        print 'region label: ', str(region_label)[2:]
                         filename_max = individual_id+"_max_"+str(int(is_threat))+"_"+str(region_label)[1:]+"_"+str(x)+"_"+str(y)
                         filename_sum = individual_id+"_sum_"+str(int(is_threat))+"_"+str(region_label)[1:]+"_"+str(x)+"_"+str(y)
                         filename_var = individual_id+"_var_"+str(int(is_threat))+"_"+str(region_label)[1:]+"_"+str(x)+"_"+str(y)
