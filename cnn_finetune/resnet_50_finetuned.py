d6dc7187bb5bee4d2acf7d3a1032d9592a8d29f3
Krista Opsahl-Ong <kristaopsahlong@gmail.com>
2017-11-13 13:27:25 -0500

    wrote something to load images from folder

--- cnn_finetune/resnet_50_finetuned.py
+++ cnn_finetune/resnet_50_finetuned.py
@@ -86,7 +86,7 @@ def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2))
     x = Activation('relu')(x)
     return x
 
-def resnet50_model(img_rows, img_cols, color_type = 1, num_classes = None):
+def resnet50_model(img_rows, img_cols, color_type=1, num_classes=None):
     """
     Resnet 50 Model for Keras
 
@@ -101,7 +101,6 @@ def resnet50_model(img_rows, img_cols, color_type = 1, num_classes = None):
       channel - 1 for grayscale, 3 for color 
       num_classes - number of class labels for our classification task
     """
-    print 'color_type: ', color_type
 
     # Handle Dimension Ordering for different backends
     global bn_axis
@@ -118,25 +117,25 @@ def resnet50_model(img_rows, img_cols, color_type = 1, num_classes = None):
     x = Activation('relu')(x)
     x = MaxPooling2D((3, 3), strides=(2, 2))(x)
 
-    x = conv_block(x, channel, [64, 64, 256], stage=2, block='a', strides=(1, 1))
-    x = identity_block(x, channel, [64, 64, 256], stage=2, block='b')
-    x = identity_block(x, channel, [64, 64, 256], stage=2, block='c')
+    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
 
-    x = conv_block(x, channel, [128, 128, 512], stage=3, block='a')
-    x = identity_block(x, channel, [128, 128, 512], stage=3, block='b')
-    x = identity_block(x, channel, [128, 128, 512], stage=3, block='c')
-    x = identity_block(x, channel, [128, 128, 512], stage=3, block='d')
+    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
 
-    x = conv_block(x, channel, [256, 256, 1024], stage=4, block='a')
-    x = identity_block(x, channel, [256, 256, 1024], stage=4, block='b')
-    x = identity_block(x, channel, [256, 256, 1024], stage=4, block='c')
-    x = identity_block(x, channel, [256, 256, 1024], stage=4, block='d')
-    x = identity_block(x, channel, [256, 256, 1024], stage=4, block='e')
-    x = identity_block(x, channel, [256, 256, 1024], stage=4, block='f')
+    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
 
-    x = conv_block(x, channel, [512, 512, 2048], stage=5, block='a')
-    x = identity_block(x, channel, [512, 512, 2048], stage=5, block='b')
-    x = identity_block(x, channel, [512, 512, 2048], stage=5, block='c')
+    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
 
     # Fully Connected Softmax Layer
     x_fc = AveragePooling2D((7, 7), name='avg_pool')(x)
