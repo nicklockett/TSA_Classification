e23c61ff5c8ba3a52aacb316994f08472e9aa3f2
Krista Opsahl-Ong <kristaopsahlong@gmail.com>
2017-11-16 00:49:14 -0500

    added in an image saving class for data generation

--- visualize/script.py
+++ visualize/script.py
@@ -3,18 +3,21 @@ from dataExtraction import *
 from random import shuffle
 from sklearn import svm
 
+
+dataset = dataCreator.CreateTensorFlowDataSetFromBlockStream(channels = 3, block_size = 58, resize = -1, segmentNumber = -100, image_filepath = "../../../rec/data/PSRC/Data/stage1/a3d/", nii_filepath = "../visualize/data/Batch_2D_warp_labels/")
+
 # need to ingest and iterate through multiple bodies
-fileId = "0e34d284cb190a79e3316d1926061bc3"
-bs = BodyScan("data/a3d/"+fileId+".a3d")
+#fileId = "5e429137784baf5646211dcc8c16ca51"
+#bs = BodyScan("data/a3d/"+fileId+".a3d")
 #bs.generate_warped_2D_segmentation(fileId)
-sc = SupervisedClassifier('../../stage1_labels.csv')
+#sc = SupervisedClassifier('../../stage1_labels.csv')
 #threat_range = sc.get_precise_threat_from_segment("0","0")
 #print threat_range
-bsg = BlockStreamGenerator(bs, sc, blockSize = 50)
-print 'about to view threat labels'
-bsg.viewThreatLabels()
+#bsg = BlockStreamGenerator(bs, sc, blockSize = 56)
+#print 'about to view threat labels'
+#bsg.viewThreatLabels()
 #block_list = bsg.generate2DBlockStreamHandLabeled()
-print 'done with stream generation'
+#print 'done with stream generation'
 #bs.plot_3d_from_blocks(block_list)
 #for block in block_list:
 #	print str(block)
