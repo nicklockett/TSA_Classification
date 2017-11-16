8ee3437eb01f9643f1f817965102f10148bf782b
Krista Opsahl-Ong <kristaopsahlong@gmail.com>
2017-11-16 01:53:10 -0500

    adding all files to have images generated

--- visualize/ImageSave.py
+++ visualize/ImageSave.py
@@ -31,8 +31,8 @@ class ImageSaver:
 			count = count + 1
 			print(count, ': about to create a body scan with filepath ', image_path)
 			bs = BodyScan(image_filepath + image_path, nii_filepath)
-	        bsg = BlockStreamGenerator(bs, self.sc, blockSize = block_size)
-	        block_list = bsg.generate2DBlockStreamHandLabeled3Channel(saveImages=True, resize = resize)
+			bsg = BlockStreamGenerator(bs, self.sc, blockSize = block_size)
+			block_list = bsg.generate2DBlockStreamHandLabeled3Channel(saveImages=True, resize = resize)
 
 	def create_train_test_arrays_from_images(save_path, X_train, Y_train, X_valid, Y_valid):
 		np.save(save_path + "X_train_56_blocksize_3_channel", X_train)
